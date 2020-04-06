3a887f7cf8e9cfccbd5dccc572b49ac2d48d6a4a
Burhan Ul Haq <burhanhaq3@gmail.com>
2020-04-05 23:39:27 -0700

    Centered grid

--- lib/game_page.dart
+++ lib/game_page.dart
@@ -6,51 +6,90 @@ class GamePage extends StatefulWidget {
 }
 
 class _GamePageState extends State<GamePage> {
+  double spacing = 3.0;
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Container(
         color: Theme.of(context).accentColor,
-        child: Column(
-          children: <Widget>[
-            SizedBox(height: 40.0),
-            Padding(
-              padding: EdgeInsets.only(top: 20),
-              child: Text(
-                'Tic-Tac-Toe',
-                style: TextStyle(
-                  color: Theme.of(context).primaryColor,
-                  fontSize: 30,
-                  decoration: TextDecoration.none,
-                  letterSpacing: 1.5,
+        child: Align(
+          alignment: Alignment.center,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              SizedBox(height: 40.0),
+              Padding(
+                padding: EdgeInsets.only(top: 20),
+                child: Text(
+                  'Tic-Tac-Toe',
+                  style: TextStyle(
+                    color: Theme.of(context).primaryColor,
+                    fontSize: 30,
+                    decoration: TextDecoration.none,
+                    letterSpacing: 1.5,
+                  ),
                 ),
               ),
-            ),
-            SizedBox(height: 30.0),
-            Flexible(
-              fit: FlexFit.loose,
-              child: GridView.count(
-                shrinkWrap: true,
-                padding: EdgeInsets.symmetric(horizontal: 40),
-                crossAxisSpacing: 3,
-                mainAxisSpacing: 3,
-                crossAxisCount: 3,
-                children: List.generate(9, (_) {
-                  return card();
-                }),
+              SizedBox(height: 30.0),
+              Container(
+                child: Stack(
+                  alignment: Alignment.center,
+                  children: <Widget>[
+                    Container(
+                      color: Colors.white,
+                      height: 250,
+                      width: 250,
+                    ),
+                    Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[
+                        Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: <Widget>[
+                            Card(),
+                            SizedBox(width: spacing),
+                            Card(),
+                            SizedBox(width: spacing),
+                            Card()
+                          ],
+                        ),
+                        SizedBox(height: spacing),
+                        Row(mainAxisSize: MainAxisSize.min, children: <Widget>[
+                          Card(),
+                          SizedBox(width: spacing),
+                          Card(),
+                          SizedBox(width: spacing),
+                          Card()
+                        ]),
+                        SizedBox(height: spacing),
+                        Row(mainAxisSize: MainAxisSize.min, children: <Widget>[
+                          Card(),
+                          SizedBox(width: spacing),
+                          Card(),
+                          SizedBox(width: spacing),
+                          Card()
+                        ]),
+                      ],
+                    ),
+                  ],
+                ),
               ),
-            )
-          ],
+            ],
+          ),
         ),
       ),
     );
   }
 }
 
-Widget card() {
-  return Container(
-    height: 5.0,
-    width: 5.0,
-    color: Colors.brown,
-  );
+class Card extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 100,
+      width: 100,
+//      color: Colors.brown,
+      color: Theme.of(context).accentColor,
+    );
+  }
 }
