b42becf53610cee8e9b2064f1b00394671e2433b
Burhan Ul Haq <burhanhaq3@gmail.com>
2020-04-07 15:51:40 -0700

    Added fading animation for bottom_row

--- lib/bottom_row.dart
+++ lib/bottom_row.dart
@@ -1,14 +1,44 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import 'constants.dart';
 import 'game_state.dart';
 
-class BottomRow extends StatelessWidget {
-  final double iconSize = 80.0;
+class BottomRow extends StatefulWidget {
+  @override
+  _BottomRowState createState() => _BottomRowState();
+}
+
+class _BottomRowState extends State<BottomRow> with TickerProviderStateMixin {
   final IconData iconP1 = Icons.close;
   final Color colorP1 = Colors.green;
   final IconData iconP2 = Icons.radio_button_unchecked;
   final Color colorP2 = Colors.red;
+  AnimationController controller;
+  AnimationController controller2;
+  Animation playingAnimation;
+  Animation noAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    controller =
+        AnimationController(vsync: this, duration: Duration(milliseconds: 300))
+          ..addListener(() => setState(() {}));
+    playingAnimation = Tween(begin: 0.5, end: 1.0).animate(controller);
+    controller2 =
+        AnimationController(vsync: this, duration: Duration(milliseconds: 400));
+    noAnimation = Tween(begin: 0.5, end: 1.0).animate(controller2);
+
+    controller.forward();
+    playingAnimation.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        controller.reverse();
+      } else if (status == AnimationStatus.dismissed) {
+        controller.forward();
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -60,18 +90,27 @@ class BottomRow extends StatelessWidget {
               ),
 //              SizedBox(width: 10),
               Container(
-                child: Icon(
-                  iconP1,
-                  color: colorP1,
-                  size: iconSize,
+                child: FadeTransition(
+                  opacity: gameState.turn == Turn.P1
+                      ? playingAnimation
+                      : noAnimation,
+                  child: Icon(
+                    iconP1,
+                    color: colorP1,
+                    size: 80,
+                  ),
                 ),
               ),
               SizedBox(width: 40),
-              Container(
-                child: Icon(
-                  iconP2,
-                  color: colorP2,
-                  size: iconSize,
+              FadeTransition(
+                opacity:
+                    gameState.turn == Turn.P2 ? playingAnimation : noAnimation,
+                child: Container(
+                  child: Icon(
+                    iconP2,
+                    color: colorP2,
+                    size: 80,
+                  ),
                 ),
               ),
               Column(
@@ -119,5 +158,12 @@ class BottomRow extends StatelessWidget {
         ],
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    controller.dispose();
+    super.dispose();
   }
 }
